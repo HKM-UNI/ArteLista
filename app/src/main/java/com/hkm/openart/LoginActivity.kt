41c68f2dc62d976304f463483122b82397a3d675
ChepeMaster <joluferadi@gmail.com>
2022-04-30 23:17:27 -0600

    Navbar, login, signup terminados att:Chepe

--- app/src/main/java/com/hkm/openart/LoginActivity.kt
+++ app/src/main/java/com/hkm/openart/LoginActivity.kt
@@ -3,6 +3,9 @@ package com.hkm.openart
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.hkm.openart.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -17,6 +20,24 @@ class LoginActivity : AppCompatActivity() {
         binding.btnNoAccount.setOnClickListener{
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            if (binding.txtEmail.text!!.isNotEmpty() && binding.txtPassword.text!!.isNotEmpty()){
+                // FirebaseAuth to register email and password
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(binding.txtEmail.text.toString(),
+                        binding.txtPassword.text.toString()).addOnCompleteListener{
+                        if (it.isSuccessful){
+                            val intent = Intent(this, MenuActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        else{
+                            Toast.makeText(this, "Correo o contraseña equivocado", Toast.LENGTH_LONG).show()
+                        }
+                    }
+            }
         }
     }
 }
--- app/src/main/java/com/hkm/openart/RegisterActivity.kt
+++ app/src/main/java/com/hkm/openart/RegisterActivity.kt
@@ -3,6 +3,10 @@ package com.hkm.openart
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isEmpty
+import androidx.core.view.isNotEmpty
+import com.google.firebase.auth.FirebaseAuth
 import com.hkm.openart.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
@@ -18,5 +22,36 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        // btn signup for account register
+        binding.btnSignUp.setOnClickListener{
+            if (binding.txtEmail.text.isNotEmpty() && checkPasswordConfirmation()){
+                // FirebaseAuth to register email and password
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(binding.txtEmail.text.toString(),
+                    binding.txtPassword.text.toString()).addOnCompleteListener{
+                        if (it.isSuccessful){
+                            Toast.makeText(this, "Registrado correctamente", Toast.LENGTH_LONG).show()
+                        }
+                        else{
+                            Toast.makeText(this, "Ha ocurrido un error", Toast.LENGTH_LONG).show()
+                        }
+                    }
+            }
+        }
+
+        binding.btnLoginFromSignUp.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun checkPasswordConfirmation(): Boolean{
+        if (binding.txtPassword.text.isNotEmpty() && binding.txtConfirmPassword.text.isNotEmpty())
+            if (binding.txtPassword.text.toString() == binding.txtConfirmPassword.text.toString())
+                return true
+
+        return false
     }
 }
