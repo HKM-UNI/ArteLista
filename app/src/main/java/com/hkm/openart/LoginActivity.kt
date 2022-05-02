c921183280df0a162983cee70a90abae83924708
ChepeMaster <joluferadi@gmail.com>
2022-05-01 18:44:32 -0600

    validaciones en login y registro agregadas att:Chepe

--- app/src/main/java/com/hkm/openart/LoginActivity.kt
+++ app/src/main/java/com/hkm/openart/LoginActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.hkm.openart.databinding.ActivityLoginBinding
 
@@ -23,7 +24,7 @@ class LoginActivity : AppCompatActivity() {
         }
 
         binding.btnLogin.setOnClickListener {
-            if (binding.txtEmail.text!!.isNotEmpty() && binding.txtPassword.text!!.isNotEmpty()){
+            if (checkEmailPassword()){
                 // FirebaseAuth to register email and password
                 FirebaseAuth.getInstance()
                     .signInWithEmailAndPassword(binding.txtEmail.text.toString(),
@@ -34,10 +35,29 @@ class LoginActivity : AppCompatActivity() {
                             finish()
                         }
                         else{
+                            binding.txtPassword.text!!.clear()
                             Toast.makeText(this, "Correo o contraseña equivocado", Toast.LENGTH_LONG).show()
                         }
                     }
             }
         }
+    }
+
+    private fun checkEmailPassword(): Boolean{
+        if (binding.txtEmail.text!!.isEmpty()){
+            binding.txtEmail.setHintTextColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_error))
+            binding.txtEmail.background.setTint(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_error))
+            Toast.makeText(this, "Llena los campos", Toast.LENGTH_LONG).show()
+            return false
+        }
+
+        if (binding.txtPassword.text!!.isEmpty()){
+            binding.txtPassword.setHintTextColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_error))
+            binding.txtPassword.background.setTint(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_error))
+            Toast.makeText(this, "Llena los campos", Toast.LENGTH_LONG).show()
+            return false
+        }
+
+        return true
     }
 }
--- app/src/main/java/com/hkm/openart/RegisterActivity.kt
+++ app/src/main/java/com/hkm/openart/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.isEmpty
 import androidx.core.view.isNotEmpty
 import com.google.firebase.auth.FirebaseAuth
@@ -25,7 +26,7 @@ class RegisterActivity : AppCompatActivity() {
 
         // btn signup for account register
         binding.btnSignUp.setOnClickListener{
-            if (binding.txtEmail.text.isNotEmpty() && checkPasswordConfirmation()){
+            if (checkEmail() && checkPasswordConfirmation()){
                 // FirebaseAuth to register email and password
                 FirebaseAuth.getInstance()
                     .createUserWithEmailAndPassword(binding.txtEmail.text.toString(),
@@ -48,10 +49,46 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun checkPasswordConfirmation(): Boolean{
-        if (binding.txtPassword.text.isNotEmpty() && binding.txtConfirmPassword.text.isNotEmpty())
-            if (binding.txtPassword.text.toString() == binding.txtConfirmPassword.text.toString())
-                return true
+        return when {
+            binding.txtPassword.text.isEmpty() -> {
+                binding.txtPassword.setHintTextColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_error))
+                binding.txtPassword.background.setTint(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_error))
+                Toast.makeText(this, "Llena los campos", Toast.LENGTH_LONG).show()
+                false
+            }
+            binding.txtConfirmPassword.text.isEmpty() -> {
+                binding.txtConfirmPassword.setHintTextColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_error))
+                binding.txtConfirmPassword.background.setTint(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_error))
+                Toast.makeText(this, "Llena los campos", Toast.LENGTH_LONG).show()
+                false
+            }
+            binding.txtPassword.text.toString() != binding.txtConfirmPassword.text.toString() -> {
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_LONG).show()
+                false
+            }
+            else -> {
+                true
+            }
+        }
+    }
 
-        return false
+    private fun checkEmail(): Boolean {
+        return when {
+            binding.txtEmail.text.isEmpty() -> {
+                binding.txtEmail.setHintTextColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_error))
+                binding.txtEmail.background.setTint(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_error))
+                Toast.makeText(this, "Llena los campos", Toast.LENGTH_LONG).show()
+                false
+            }
+            android.util.Patterns.EMAIL_ADDRESS.matcher(binding.txtEmail.text.toString()).matches() -> {
+                true
+            }
+            else -> {
+                Toast.makeText(this, "Correo inválido", Toast.LENGTH_LONG).show()
+                binding.txtEmail.background.setTint(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_error))
+                binding.txtEmail.setHintTextColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_error))
+                false
+            }
+        }
     }
 }
